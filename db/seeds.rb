a1956ebb6b53abbce5ec8382695594fe97640bcf
darkrai0101 <nv.trung91@gmail.com>
2016-10-15 19:18:25 +0700

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@
 # OrderStatus.create! id: 3, name: "Shipped"
 # OrderStatus.create! id: 4, name: "Cancelled"
 
-Section.create!  id: 1, name: "Drinks"
-Section.create!  id: 2, name: "Breakfast"
-Section.create!  id: 3, name: "Lunch"
-Section.create!  id: 4, name: "Dinner"
+# Section.create!  id: 1, name: "Drinks"
+# Section.create!  id: 2, name: "Breakfast"
+# Section.create!  id: 3, name: "Lunch"
+# Section.create!  id: 4, name: "Dinner"
 
 FoodItem.create id: 1, name: "SmileMee - Bánh Mì Chảo", description: "Chảo cười đặc biệt gì đó thì gồm có 1 miếng thịt bò tương đối to, 1 miếng pate khá to, 1 quả trứng ốp la, xúc xích, khoai nghiền và 1 ít rau ăn kèm bên trên. Về cảm nhận chung thì khá bình thường, ăn được nhưng vị không có gì đặc sắc. Thịt ăn mềm, hơi nhạt. Khoai nghiền khá ngon, món nổi bật nhất trog cả chảo. Pate cũng khá ngon. Trứng lòng đào đủ độ. Nhưng rau thì thực sự rất tệ.", price: 12000, section_id: 2, image_url: "https://media.foody.vn/res/g15/142432/prof/s480x300/foody-mobile-smilemee2-jpg-629-635788977984404958.jpg"
 FoodItem.create id: 2, name: "Bánh Mì Sốt Vang", description: "ước sốt được đổ vào hầm với thịt, và các gia vị khác, cùng với một ít bột năng để tạo độ sánh cần thiết. Khi nước sốt đã sánh đặc vừa phải, đầu bếp cho thêm rau húng Láng, mùi ta, hành thái nhỏ để cho dậy mùi. Bánh mì ăn kèm phải đảm bảo nóng, giòn vàng ươm thì đặt cạnh bát bò sốt vang mới ngon và bắt mắt được.", price: 25000, section_id: 2, image_url: "https://media.foody.vn/res/g2/19123/prof/s480x300/foody-mobile-banh-mi-jpg-614-636016044824903869.jpg"
