46928af60de269b2b454ce556d83de3414a5d7a2
darkrai0101 <nv.trung91@gmail.com>
2016-10-16 17:08:21 +0700

    fix not clear current order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -2,13 +2,15 @@ class OrderController < ApplicationController
   def update
   	@fee_ship = 20000;
   	@order = current_order
+    @order.discount = 0
 
   	@order_items = @order.order_items
   	if params[:coupon].present? and params[:coupon] === "CODERSCHOOL"
-  		@order.discount =  @order.subtotal * 50/100
-  		@order.total = @order.subtotal - @order.discount + @fee_ship
+  		@order.discount =  @order.subtotal * 50/100  		
   	end
+    @order.total = @order.subtotal - @order.discount + @fee_ship    
   	@order.update(order_params)
+    session[:order_id] = nil
   end
 private
   def order_params
