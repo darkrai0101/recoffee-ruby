48704791d13213adcc220ec0a3eada20d5a37ebf
darkrai0101 <nv.trung91@gmail.com>
2016-10-16 15:29:48 +0700

    add counpon

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,7 +1,13 @@
 class OrderController < ApplicationController
   def update
+  	@fee_ship = 20000;
   	@order = current_order
+
   	@order_items = @order.order_items
+  	if params[:coupon].present? and params[:coupon] === "CODERSCHOOL"
+  		@order.discount =  @order.subtotal * 50/100
+  		@order.total = @order.subtotal - @order.discount + @fee_ship
+  	end
   	@order.update(order_params)
   end
 private
